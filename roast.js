b506c2ee4c3b02dc0069ff6d34bdaf755f69ba40
KayXD <kayxddddd@gmail.com>
2021-10-28 20:00:33 +0800

    Update roast.js

--- roast.js
+++ roast.js
@@ -5,4 +5,12 @@ function roast() {
 	  alert('Input is required.');
 	  return;
 	}
-}
+  }
+  var roastnum = Math.floor(Math.random() * (1 - 0) + 0));
+  switch (roastnum) {
+    case 0:
+			alert("Hey" + victim + ",");
+			break;
+		case 1:
+			alert("");
+			break;

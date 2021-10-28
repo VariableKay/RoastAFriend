aba227317e83ee9170cf73ca30b3484044c33297
KayXD <kayxddddd@gmail.com>
2021-10-28 16:11:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-let input = document.querySelector('#input');
 
-searchBtn.addEventListener('click', function(e) {
-	let word = input.value;
-	if (word === '') {
-		alert('Input is required!');
-		return;
-	}
-});

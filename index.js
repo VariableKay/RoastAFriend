12b47eda23c83c58b72c29c4975d3e8352bf032f
KayXD <kayxddddd@gmail.com>
2021-10-28 16:03:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,18 +24,17 @@ searchBtn.addEventListener('click', function(e) {
 });
 async function getData(word) {
 	loading.style.display = 'block';
-	// Ajax call
+
 	const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${apiKey}`);
 	const data = await response.json();
 	console.log(data);
-	// if empty result
+
 	if (!data.length) {
 		loading.style.display = 'none';
 		notFound.innerText = ' No results found';
 		return;
 	}
 
-	// If result is suggetions
 	if (typeof data[0] === 'string') {
 		loading.style.display = 'none';
 		let heading = document.createElement('h3');
@@ -50,22 +49,18 @@ async function getData(word) {
 		return;
 	}
 
-	// Result found
 	loading.style.display = 'none';
 	let defination = data[0].shortdef[0];
 	defBox.innerText = defination;
 
-	// Sound
 	const soundName = data[0].hwi.prs[0].sound.audio;
 	if (soundName) {
 		renderSound(soundName);
 	}
 
-	// console.log(data);
 }
 
 function renderSound(soundName) {
-	// https://media.merriam-webster.com/soundc11
 	let subfolder = soundName.charAt(0);
 	let soundSrc = `https://media.merriam-webster.com/soundc11/${subfolder}/${soundName}.wav?key=${apiKey}`;
 

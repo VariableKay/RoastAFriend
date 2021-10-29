e879c2c8cce6224263a918fac968ec789e018b3a
KayXD <kayxddddd@gmail.com>
2021-10-29 17:30:42 +0800

    Update roast.js

--- roast.js
+++ roast.js
@@ -5,3 +5,88 @@ function roast() {
 	  alert("Input is required.");
 	  return;
   }
+    var roastnum = Math.floor(Math.random() * (26 - 0) + 0);
+    switch (roastnum) {
+    case 0:
+		alert("Hey " + victim + ", " + "yo mama so dumb she gave birth to a joke!");
+		break;
+	case 1:
+		alert("Hey " + victim + ", " + "yo mama so fat they made her play the role of the iceberg in Titanic!");
+		break;
+	case 2:
+		alert("Hey " + victim + ", " + "yo mama so fat NASA mistook her for a planet!");
+		break;
+	case 3:
+		alert("Hey " + victim + ", " + "you're so hated that you were probably behind YouTube Rewind 2018!");
+		break;
+	case 4:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 5:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 6:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 7:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 8:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 9:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 10:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 11:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 12:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 13:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 14:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 15:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 16:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 17:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 18:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 19:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 20:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 21:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 22:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 23:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 24:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 25:
+		alert("Hey " + victim + ", " + "");
+		break;
+	case 26:
+		alert("Hey " + victim + ", " + "");
+		break;
+	}
+}
